d1416ed3f5be0c7a7a5edb82cd49f66cad60a7bf
Mike <mikewrubel51@gmail.com>
2021-11-02 16:17:53 -0400

    commit tree 3521a133d8f4c34fc4d32312355279831f884b8c

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+require 'pry'
+class CashRegister
+    attr_accessor :total
+    def initialize(employee_discount)
+        @total = 0
+    end
 
+    def add_item(title, price)
+    end
+
+    def apply_discount
+    end
+
+    def items(item1, item2, item3)
+    end
+
+    def void_last_transaction
+    end
+
+    def total
+        #binding.pry
+        @total
+    end
+end
